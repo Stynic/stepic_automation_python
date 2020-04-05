4723522b82e0276d6fac4cd9e9237ddb91f974e7
Daniil Kraev <dv.kraev@jet.su>
2020-04-05 16:43:24 +0300

    На папке test_project проверял ,какие pytest тесты запускает(Оказывается он обращается внимание только на тесты,у которых имя класса начинается с test*.py b *test.py , в название класса также первым должно стоять слово Test и в названия методов также начинаются со слова test).

---
+++ test_project/test_regression.py
@@ -0,0 +1,20 @@
+class TestLessonCreate():
+    # номер 5
+    def test_create_lesson(self, browser):
+        pass
+
+
+    # номер 6
+    def user_with_lesson_can_create_lesson_from_navbar_test(self, browser):
+        pass
+
+
+class TestCourseCreate():
+    # номер 7
+    def test_create_course(self, browser):
+        pass
+
+
+# номер 8
+def uest_can_open_new_course(browser):
+    pass
